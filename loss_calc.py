f1764c9c9bd5d263a78fe3f24d4aa01c64ecf6e2
sotramk <sotramk@gmail.com>
2019-07-29 11:04:29 -0400

    update to lcTest

--- loss_calc.py
+++ loss_calc.py
@@ -1,26 +1,29 @@
 #!/bin/python
 # rewrite of loss_calc.py
+# Program to calculate profit goals and stoploss points from a buy point.
 # written 07/2019
+
+# Get stock symbol and purchase  price from user.
+# Print output to screen.
 
 stk = input('Stock symbol?')
 prompt = 'What is the buy_price price?\n'
 buy_price = input(prompt)
 buy_price = float(buy_price)
 firstTarget = buy_price * 1.05
-firstTarget = round(firstTarget,2)
 secoundTarget = buy_price  * 1.10
-secoundTarget = round(secoundTarget,2)
 thirdTarget = buy_price * 1.23
-thirdTarget = round(thirdTarget,2)
 print('For a purchase price of ',buy_price)
-print('First target ',firstTarget)
-txt1 = "First target -- {}".format(firstTarget)
-print('Secound target ',secoundTarget)
-txt2 = "Secound target -- {}".format(secoundTarget)
-print('Third target ',thirdTarget)
-txt3 = "Third target -- {}".format(thirdTarget)
+txt1 = "First target -- {:.2f}".format(firstTarget)
+print(txt1)
+txt2 = "Secound target -- {:.2f}".format(secoundTarget)
+print(txt2)
+txt3 = "Third target -- {:.2f}".format(thirdTarget)
+print(txt3)
 pct = ( (.97, .96, .95, .94, .93, .92) )
 y = 3
+
+# Write to a text file.
 
 f = open("lcTest.txt", "a")
 f.write("\n")
@@ -45,14 +48,16 @@ for i in pct:
     y = y + 1
 f.close()
 
-# Program to calculate the spread between prices
+# Program to calculate the spread between prices.
 
-# Get information from user
+# Get information from user.
+
 prompt = 'What is the current price?\n'
 target_price = input(prompt)
 target_price = float(target_price)
 
-# calculate percentage difference of prices
+# Calculate percentage difference of prices.
+
 if buy_price > target_price:
     result = (buy_price / target_price - 1) * 100
     result = float(result)
