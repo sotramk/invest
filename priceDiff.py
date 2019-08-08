24f9d7ac7c542e39888da4c394f0cd6ea6f4cda9
sotramk <sotramk@gmail.com>
2019-08-08 17:25:02 -0400

    wrote as a function

--- priceDiff.py
+++ priceDiff.py
@@ -4,20 +4,26 @@
 
 # Get information from user
 
-prompt = 'What is the current price?\n'
-currentPrice = input(prompt)
-currentPrice = float(currentPrice)
-prompt = 'What is the target price?\n'
-targetPrice = input(prompt)
-targetPrice = float(targetPrice)
+def inpPD():
+    prompt = 'What is the current price?\n'
+    currentPrice = input(prompt)
+    if currentPrice == '':
+       quit
+    else:
+        currentPrice = float(currentPrice)
+        prompt = 'What is the target price?\n'
+        targetPrice = input(prompt)
+        targetPrice = float(targetPrice)
 
 # calculate percentage difference of prices
 
-if currentPrice > targetPrice:
-    result = (currentPrice / targetPrice - 1) * 100
-    result = float(result)
-else:
-    result = (targetPrice / currentPrice - 1) * 100
-    result = float(result)
-print('Difference is ',round(result,2),' percent')
+        if currentPrice > targetPrice:
+            result = (currentPrice / targetPrice - 1) * 100
+            result = float(result)
+        else:
+            result = (targetPrice / currentPrice - 1) * 100
+            result = float(result)
+        print('Difference is ',round(result,2),' percent\n')
+        inpPD()
+inpPD()
 
