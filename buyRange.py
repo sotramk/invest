c906d0ed91fa2d9331532a340b20feec1c012339
Kevin Martos <sotramk@gmail.com>
2020-01-15 18:40:42 -0500

    fixed write file to the Desktop

--- buyRange.py
+++ buyRange.py
@@ -8,14 +8,15 @@ def stkData():
     ni = i.upper()
     b = input('Buy point? ')
     t = float(b) * 1.05
-    t = round(t,2)
+    t = round(t, 2)
     print(str(i) + ' -- ' + str(b) + ' -- ' + str(t))
     txt = str(ni) + ' -- ' + str(b) + ' -- ' + str(t)
     f.write(str(txt))
     f.write("\n")
     f.write("\n")
 
-f = open("/home/sot/buyRange.txt", "a")
+
+f = open("/home/sot/Desktop/buyRange.txt", "a")
 while True:
     i = input('Symbol? ')
     if i == '':
