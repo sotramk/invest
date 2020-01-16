7a065dee7e3143415d93fb08d52ce64437ff37e4
Kevin Martos <sotramk@gmail.com>
2020-01-16 13:13:54 -0500

    fix float issue

--- lcTest.py
+++ lcTest.py
@@ -9,24 +9,27 @@
 stk = input('Stock symbol?')
 prompt = 'What is the buy_price price?\n'
 buy_price = float(input(prompt))
-firstTarget = buy_price * 1.05
-secoundTarget = buy_price  * 1.10
-thirdTarget = buy_price * 1.23
-print('For a purchase price of ',buy_price)
-txt1 = "First target -- {:.2f}".format(firstTarget)
-print(txt1)
-txt2 = "Secound target -- {:.2f}".format(secoundTarget)
-print(txt2)
-txt3 = "Third target -- {:.2f}".format(thirdTarget)
-print(txt3)
-pct = ( (.97, .96, .95, .94, .93, .92) )
-y = 3
+if buy_price == '':
+    quit
+else:
+    firstTarget = buy_price * 1.05
+    secoundTarget = buy_price * 1.10
+    thirdTarget = buy_price * 1.23
+    print('For a purchase price of ', buy_price)
+    txt1 = "First target -- {:.2f}".format(firstTarget)
+    print(txt1)
+    txt2 = "Secound target -- {:.2f}".format(secoundTarget)
+    print(txt2)
+    txt3 = "Third target -- {:.2f}".format(thirdTarget)
+    print(txt3)
+    pct = ((.97, .96, .95, .94, .93, .92))
+    y = 3
 
 # Write to a text file.
 
 f = open("/home/sot/Documents/invest/lcTest.txt", "a")
 f.write("\n")
-txt = "{} -- {}".format(stk.upper(),buy_price)
+txt = "{} -- {}".format(stk.upper(), buy_price)
 f.write(txt)
 f.write("\n")
 f.write(str(txt1))
@@ -39,8 +42,8 @@ f.write("\n")
 for i in pct:
     x = i * buy_price
     x = float(x)
-    x = round(x,2)
-    print(y,'% --',round(x,2))
+    x = round(x, 2)
+    print(y, '% --', round(x, 2))
     txt = str(y) + '% --' + str(x)
     f.write(str(txt))
     f.write("\n")
@@ -53,7 +56,7 @@ f.close()
 
 prompt = 'What is the current price?\n'
 target_price = input(prompt)
-target_price = float(target_price)
+target_price = float(input(prompt))
 
 # Calculate percentage difference of prices.
 
@@ -63,5 +66,4 @@ if buy_price > target_price:
 else:
     result = (target_price / buy_price - 1) * 100
     result = float(result)
-print('Difference is ',round(result,2),' percent')
-
+ print('Difference is ', round(result, 2), ' percent')
